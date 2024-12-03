922f1235d2c6ff403a4ad0ae951ad8c4f86766d4
Jamie Kelly <git@jamiekelly.com>
2024-12-03 07:12:02 +0000

    Refactor lib run to remove double solver instantiation

--- src/lib.rs
+++ src/lib.rs
@@ -3,11 +3,10 @@ pub mod solvers;
 use crate::solvers::*;
 
 pub fn run() {
-    println!("Day 01 Part One: {}", Day01Solver{
+    let day_01_solver = Day01Solver{
         input_path: String::from("puzzle-inputs/day01.txt")
-    }.solve_part_one());
-    println!("Day 01 Part Two: {}", Day01Solver{
-        input_path: String::from("puzzle-inputs/day01.txt")
-    }.solve_part_two());
+    };
+    println!("Day 01 Part One: {}", day_01_solver.solve_part_one());
+    println!("Day 01 Part Two: {}", day_01_solver.solve_part_two());
 }
 
